0e1052f7e32b8aeca6a6321c84506a825f8d51bd
Jonathan D Kaonga <jonthedev0@gmail.com>
2024-02-12 18:51:22 +0100

    feat: create reusable Button component
    feat: add Mark all as complete button to ButtonGroup template
    feat: add Mark all as incomplete button to ButtonGroup template
    feat: add Reset to initial button to ButtonGroup template
    feat: add Remove all items button to ButtonGroup template
    
    style: add Button modular styles

--- src/components/Feat/AddItemForm/AddItemForm.jsx
+++ src/components/Feat/AddItemForm/AddItemForm.jsx
@@ -1,11 +1,12 @@
 import styles from "./AddItemForm.module.css"
+import { Button } from "../Button/Button"
 
 export const AddItemForm = () => {
   return (
     <form className={styles["add-item-form"]}>
       <h2>Add an item</h2>
       <input type="text" />
-      <button className={styles.btn}>Add to list</button>
+      <Button>Add To List</Button>
     </form>
   )
 }
--- src/components/Feat/ButtonGroup/ButtonGroup.jsx
+++ src/components/Feat/ButtonGroup/ButtonGroup.jsx
@@ -1,5 +1,13 @@
+import { Button } from "../Button/Button"
 import styles from "./ButtonGroup.module.css"
 
 export const ButtonGroup = () => {
-  return <div className={styles["button-group"]}>ButtonGroup</div>
+  return (
+    <div className={styles["button-group"]}>
+      <Button buttonType="secondary">Mark all as complete</Button>
+      <Button buttonType="secondary">Mark all as incomplete</Button>
+      <Button buttonType="secondary">Reset to initial</Button>
+      <Button buttonType="secondary">Remove all items</Button>
+    </div>
+  )
 }
