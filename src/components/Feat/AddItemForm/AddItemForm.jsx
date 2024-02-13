09151fd01ca37b5f2a91a261013e02ca1973b0b0
Jonathan D Kaonga <jonthedev0@gmail.com>
2024-02-13 19:17:32 +0100

    feat: auto focus on add item input if validation fails

--- src/components/Feat/AddItemForm/AddItemForm.jsx
+++ src/components/Feat/AddItemForm/AddItemForm.jsx
@@ -1,15 +1,17 @@
 import styles from "./AddItemForm.module.css"
 import { Button } from "../Button/Button"
-import { useState } from "react"
+import { useRef, useState } from "react"
 
 export const AddItemForm = ({ setItems }) => {
   const [itemText, setItemText] = useState("")
+  const inputRef = useRef()
 
   const handleSubmit = (e) => {
     e.preventDefault()
 
     if (!itemText) {
       alert("Field can not be empty!")
+      inputRef.current.focus()
       return
     }
 
@@ -25,6 +27,7 @@ export const AddItemForm = ({ setItems }) => {
     <form onSubmit={handleSubmit} className={styles["add-item-form"]}>
       <h2>Add an item</h2>
       <input
+        ref={inputRef}
         type="text"
         value={itemText}
         onChange={(e) => setItemText(e.target.value)}
