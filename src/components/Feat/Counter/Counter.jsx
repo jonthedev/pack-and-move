c672b155bed8ae2c4bac176872f896654ce199da
Jonathan D Kaonga <jonthedev0@gmail.com>
2024-02-14 20:26:11 +0100

    chore: remove console.log

--- src/components/Feat/Counter/Counter.jsx
+++ src/components/Feat/Counter/Counter.jsx
@@ -1,7 +1,6 @@
 import styles from "./Counter.module.css"
 
 export const Counter = ({ numberOfItemsPacked, totalNumberOfItems }) => {
-  console.log(numberOfItemsPacked)
   return (
     <div className={styles.counter}>
       {numberOfItemsPacked} / {totalNumberOfItems} items packed
