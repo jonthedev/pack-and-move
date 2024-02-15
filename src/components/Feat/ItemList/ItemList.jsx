7283efb1a57690ba513cd818cf37cbaccc55eda1
Jonathan D Kaonga <jonthedev0@gmail.com>
2024-02-15 06:59:30 +0100

    chore: move select component logic into ItemList component
    feat: implement sorting logic for items

--- src/components/Feat/ItemList/ItemList.jsx
+++ src/components/Feat/ItemList/ItemList.jsx
@@ -1,13 +1,51 @@
-import EmptyView from "../Empty/EmptyView"
-import { ReactSelect } from "../Select/Select"
 import styles from "./ItemList.module.css"
+import Select from "react-select"
+import EmptyView from "../Empty/EmptyView"
+import { useState } from "react"
+
+const sortingOptions = [
+  {
+    label: "Sort by default",
+    value: "default"
+  },
+  {
+    label: "Sort by packed",
+    value: "packed"
+  },
+  {
+    label: "Sort by unpacked",
+    value: "unpacked"
+  }
+]
 
 export const ItemList = ({ items, handleDeleteItem, handleToggleItem }) => {
+  const [sortBy, setSortBy] = useState("default")
+
+  const sortedItems = [...items].sort((a, b) => {
+    if (sortBy == "packed") {
+      return b.packed - a.packed
+    }
+    if (sortBy == "unpacked") {
+      return a.packed - b.packed
+    }
+    return
+  })
+
   return (
     <ul className={styles["item-list"]}>
       {items.length === 0 && <EmptyView />}
-      {items.length > 0 ? <ReactSelect /> : null}
-      {items.map((item) => (
+      {items.length > 0 ? (
+        <section className={styles["sorting"]}>
+          <Select
+            onChange={(option) => {
+              setSortBy(option.value)
+            }}
+            defaultValue={sortingOptions[0]}
+            options={sortingOptions}
+          />
+        </section>
+      ) : null}
+      {sortedItems.map((item) => (
         <Item
           key={item.name}
           item={item}
